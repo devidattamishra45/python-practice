120a394c1e282f97e1ad2417c7daa7efc5ac4eb2
devidatta mishra <d.mishra4525@gmail.com>
2025-09-21 12:55:24 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,14 @@
+print("hello world")
+name="sachin"
+print(name)
+print(type(10))
+name1=int(input("what iass the best things--"))
+print(name1)
+print("hello\nworld")
+print("this isa tab\tcharacters.")
+print("this isa tab\\characters.")
+print("this isa tab\"characters.")
+print("this isa tab\'characters.")
+print("hello","world",sep=",",end="!\n")
+fruits=["apple","banana","chery"]
+print("apple"in fruits)
